401fa09fac938091d778684baca2c9b6ead5c4a5
emmanuelcafu <emmauelcafu@gmail.com>
2023-11-05 12:44:20 -0500

    actualizacion index

---
+++ index.php
@@ -0,0 +1,27 @@
+<?php
+
+
+$array = [
+    1 => "a",
+    "hola" => "b"
+];
+$my_string = "esto es una string";
+// echo $array;
+
+const MY_CONSTANTE = "el valor es una constante";
+const MY_CONSTANTEs = "142";
+echo MY_CONSTANTE. "\n";
+echo MY_CONSTANTEs. "\n";
+$my_array= ["hola",51,"sehunfo"];
+echo $my_array[1];
+array_push($my_array,"añadido");
+print_r($my_array);
+
+for ($index=0; $index <=10; $index ++){
+    echo $index;
+}
+function nombre(){
+    echo 10;
+};
+
+nombre();
